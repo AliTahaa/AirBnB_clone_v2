9fe75a4fd5ba5ce2c2bcdfc8bb6165c41a0f7fa3
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-07-15 02:42:41 +0300

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,34 @@
 #!/usr/bin/env bash
-# sets up your web servers for the deployment of web_static
+# Set up a web server for deployment of web_static.
 
 apt-get update
-apt-get -y install nginx
+apt-get install -y nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>' > /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -hR ubuntu:ubuntu /data/
-sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 service nginx restart
