39c0825d044d98a6d12eed201565ec68589ffbea
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-07-15 09:31:45 +0300

    try

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # sets up web servers for deployment of web_static
 
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "hello world" | sudo tee /data/web_static/releases/test/index.html
