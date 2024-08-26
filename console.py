8c3aa473a88d6a8674cb4a11f1e573c9cb3d4a6d
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-08-26 06:23:21 +0300

    fix

--- console.py
+++ console.py
@@ -144,9 +144,9 @@ class HBNBCommand(cmd.Cmd):
         if len(args) == 0:
             print("** class name missing **")
             return False
-        if args[0] in HBNBCommand.classes:
+        if args[0] in classes:
             new_dict = self._key_value_parser(args[1:])
-            instance = HBNBCommand.classes[args[0]](**new_dict)
+            instance = classes[args[0]](**new_dict)
         else:
             print("** class doesn't exist **")
             return False
--- models/city.py
+++ models/city.py
@@ -3,15 +3,16 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from os import getenv
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """Representation of city """
     __tablename__ = 'cities'
-
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
     else:
         state_id = ""
         name = ""
