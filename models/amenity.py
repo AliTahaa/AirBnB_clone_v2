f001a931ada42aac4be15102eba2372bce5148d8
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-09-07 08:58:06 +0300

    task 8

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,21 @@
-#!/usr/bin/python3
-""" State Module for HBNB project """
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
 from models.base_model import BaseModel, Base
 from os import getenv
-from sqlalchemy import Column, String, ForeignKey, Float, Integer, Table
+import sqlalchemy
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """ Amenity class """
-    __tablename__ = 'amenities'
+    """Representation of Amenity """
     if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary="place_amenity")
     else:
         name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
--- models/state.py
+++ models/state.py
@@ -9,10 +9,10 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship("City", backref="state", cascade="all, delete")
+        name = Column(String(128), nullable=False)
     else:
         @property
         def cities(self):
