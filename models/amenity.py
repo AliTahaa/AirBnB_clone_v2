c77ecab278bc9153893e7c1d9c8cdf7dfeae5739
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-09-07 09:45:35 +0300

    not any thing new but try

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,15 @@
-#!/usr/bin/python
-""" holds class Amenity"""
-import models
+#!/usr/bin/python3
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey, Float, Integer, Table
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
+    """ Amenity class """
+    __tablename__ = 'amenities'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
     else:
         name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
